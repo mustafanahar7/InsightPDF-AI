eac29a52365a779a34ab18d527e4e8e6dc7da12a
mustafanahar7 <naharmustafa7@gmail.com>
2025-05-13 16:36:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.chains import create_history_aware_retriever , create_retrieval_c
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-from langchain_ollama import OllamaEmbeddings
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
