7da96a989f01c578612f8e6d7752474f81494baa
mustafanahar7 <naharmustafa7@gmail.com>
2025-05-14 16:22:31 +0530

    major changes in pj

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_groq import ChatGroq
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader , TextLoader , UnstructuredWordDocumentLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.callbacks import StreamlitCallbackHandler
@@ -18,48 +18,42 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 os.environ['GROQ_API_KEY']= st.secrets["GROQ_API_KEY"]
-st.set_page_config("InsightPDF AI",page_icon="📄")
+
+########################## Headings ########################## 
 st.markdown("""
     <h1 style='text-align: center; color: #4A90E2; font-family: "Segoe UI", sans-serif;'>
         📄 InsightPDF AI
     </h1>
     <p style='text-align: center; color: gray; font-size: 18px;'>
-        Upload your PDF and ask questions in natural language
+        Upload your Document and ask questions in natural language
     </p>
     <strong>Do not upload sensitive or confidential files.</strong> This tool is for general document analysis only.
     
 """, unsafe_allow_html=True)
+st.sidebar.title("Upload File")
+#################### ***************** #######################
+
+
+########################## Check session store and file processing ##########################
+if 'store' not in st.session_state:
+    st.session_state.store={}
+
+if 'is_file_processed' not in st.session_state:
+    st.session_state.is_file_processed=False
+#################### ***************** #######################    
 
 def get_session_history(session_id:str)->BaseChatMessageHistory:
             if session_id not in st.session_state.store:
                 st.session_state.store[session_id]=ChatMessageHistory()
             return st.session_state.store[session_id]
-        
-## Groq
-# api_key = st.sidebar.text_input("Enter Groq key",type='password')
-
-# if api_key:
-# llm = ChatGroq(model="Gemma2-9b-It",groq_api_key=api_key)
+    
+########################## Select and initial LLM Model ##########################
 select_model = st.sidebar.selectbox(label="Choose the model",options=["Gemma2-9b-It","Llama3-8b-8192","Llama3-70b-8192"])
 llm = ChatGroq(model=select_model)
+#################### ***************** #######################
 
-#set title of sidebar
-st.sidebar.title("Upload File")
-## Get the session name
-# session_id= st.sidebar.text_input("Session ID",value="default_session")
 
-## Check session store
-if 'store' not in st.session_state:
-    st.session_state.store={}
-    
-## Check File procession
-if 'is_file_processed' not in st.session_state:
-    st.session_state.is_file_processed=False
-    
-upload_file = st.sidebar.file_uploader("choose a pdf file",type="pdf",accept_multiple_files=True)
-
-##### LLM Design 
-
+########################## Prompt Design ##########################
 contextualize_system_prompt ="""
     Give a chat history and latest user question
     which might reference context in chat history ,
@@ -68,7 +62,6 @@ contextualize_system_prompt ="""
     just Reformulate it if needed otherwise give as it is
 
 """
-
 contextualize_prompt = ChatPromptTemplate.from_messages([
     ('system',contextualize_system_prompt),
     MessagesPlaceholder('chat_history'),
@@ -76,15 +69,13 @@ contextualize_prompt = ChatPromptTemplate.from_messages([
 ])
 
 ## Question Answer
-
 system_prompt =(
 "You are an assistant for question-answer task ."
 "Use the Following piece of context from Retrieved Document to answer the question "
-" If You don't know the answer simply say don't know."
+" If You don't know the answer simply say don't know ."
 "\n\n"
 "{context}"
 )
-
 
 qa_prompt = ChatPromptTemplate.from_messages([
     ('system',system_prompt),
@@ -92,17 +83,33 @@ qa_prompt = ChatPromptTemplate.from_messages([
     ('human',"{input}")
 ])
 
+#################### ******** End of Prompt Design********* #######################
+
+########################## Upload File and Processing ##########################
+upload_file = st.sidebar.file_uploader("choose a file",type=["pdf","txt","docx"],accept_multiple_files=True)
 def process_file_and_create_chain():
     document=[]
     for uploaded_file in upload_file:
-        temppdf = f"./temp.pdf"
-        with open(temppdf,"wb") as file:
-            file.write(uploaded_file.getvalue())
-            file_name = uploaded_file.name
-    
-        loader = PyPDFLoader(temppdf)
-        docs = loader.load()
-        document.extend(docs)
+        suffix = os.path.splitext(uploaded_file.name)[-1].lower()
+
+        temp_path = f"./temp{suffix}"
+        with open(temp_path, "wb") as f:
+            f.write(uploaded_file.getvalue())
+
+        # Choose appropriate loader
+        if suffix == ".pdf":
+            loader = PyPDFLoader(temp_path)
+        elif suffix == ".txt":
+            loader = TextLoader(temp_path)
+        elif suffix == ".docx":
+            loader = UnstructuredWordDocumentLoader(temp_path)
+        else:
+            st.error("Unsupported file format. Only PDF and TXT files are supported.")
+            continue
+        
+        if loader:
+            docs = loader.load()
+            document.extend(docs)
         
     with st.spinner("Analyzing Files ..."):
         ## Create chunks and store in vector db
@@ -111,22 +118,11 @@ def process_file_and_create_chain():
         
         embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
         db = FAISS.from_documents(chunk_docs,embedding)
-        retriever = db.as_retriever()
+        st.session_state.retriever = db.as_retriever()
         # st.stop()
     
-    ## Create retrieval Chain 
-    history_aware_retriever = create_history_aware_retriever(llm , retriever ,contextualize_prompt)
-    question_answer_chain = create_stuff_documents_chain(llm,qa_prompt)
-    rag_chain = create_retrieval_chain(history_aware_retriever,question_answer_chain)
-
-    st.session_state.conversational_rag_chain = RunnableWithMessageHistory(
-    rag_chain,get_session_history,
-    input_messages_key = "input",
-    history_messages_key="chat_history",
-    output_messages_key= "answer"
-    )
-
     st.session_state.is_file_processed=True
+    return st.session_state.retriever
 
 def generate_response(user_input):
     session_id = "default"
@@ -140,8 +136,19 @@ def generate_response(user_input):
         st.write(response['answer'])    
 
 
-if upload_file and not st.session_state.is_file_processed:
-    process_file_and_create_chain()
+if upload_file:
+    retriever = process_file_and_create_chain()
+    ## Create retrieval Chain 
+    history_aware_retriever = create_history_aware_retriever(llm , retriever ,contextualize_prompt)
+    question_answer_chain = create_stuff_documents_chain(llm,qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever,question_answer_chain)
+
+    st.session_state.conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,get_session_history,
+    input_messages_key = "input",
+    history_messages_key="chat_history",
+    output_messages_key= "answer"
+    )
         
 if st.session_state.is_file_processed:
     if "messages" not in st.session_state:
@@ -155,7 +162,5 @@ if st.session_state.is_file_processed:
         generate_response(user_input)
     
 else:
-    st.spinner("PDF Under-process")
+    st.spinner("Document Under-process")
     
-# else:
-# st.info("Enter Your API Key")        
